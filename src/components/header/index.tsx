970cdc4aded64745135aac1dca8473bba25d9575
张章 <hzzhangzhang1@corp.netease.com>
2022-01-11 16:03:13 +0800

    style: 样式

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
-import Icon from '@ant-design/icons';
-import { Logo } from '@/assets/icon';
 import './index.less';
 
 function Header(props: any) {
     return (
         <nav className="g-header-nav">
-            <Icon style={{ textAlign: 'left' }} component={Logo} />
-            实体抽取工具
+            <div className="g-header-nav-logo">实体抽取工具</div>
         </nav>
     );
 }

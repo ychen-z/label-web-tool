c78cfb05e92c29ba4b9b35d71b097326ab329629
张章 <hzzhangzhang1@corp.netease.com>
2022-01-12 14:32:16 +0800

    feature: tool-page 页面结构

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -5,7 +5,8 @@ import './index.less';
 function Header(props: any) {
     return (
         <nav className="g-header-nav">
-            <div className="g-header-nav-logo">实体抽取工具</div>
+            <div className="g-header-nav-logo" />
+            <div className="g-header-nav-title">实体抽取工具</div>
         </nav>
     );
 }
--- src/view/tool-page/index.tsx
+++ src/view/tool-page/index.tsx
@@ -1,5 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './index.less';
 
-export default function index() {
-    return <div>工具页面</div>;
+const Btns = [
+    {
+        name: 'a',
+        type: 'A',
+        top: 0
+    },
+    {
+        name: 'b',
+        type: 'B',
+        top: 100
+    },
+    {
+        name: 'c',
+        type: 'C',
+        top: 200
+    },
+    {
+        name: 'd',
+        type: 'D',
+        top: 300
+    },
+    {
+        name: 'e',
+        type: 'E',
+        top: 400
+    },
+    {
+        name: 'f',
+        type: 'F',
+        top: 500
+    }
+];
+export default function ToolPage() {
+    const [state, setState] = useState('A');
+    return (
+        <div className="m-tool-page">
+            <section className="left">
+                <div className="content">
+                    {Btns.map(item => (
+                        <span className={`item ${state == item.type ? 'active' : ''}`} style={{ top: item.top }} onClick={() => setState(item.type)}>
+                            {item.name}
+                        </span>
+                    ))}
+                </div>
+            </section>
+            <section className="right">右边 {state}</section>
+        </div>
+    );
 }

a32a01e61c01f4b1a929a07ac999db38c61f7513
xiaozhanglang <itiswell@163.com>
2022-07-15 11:53:58 +0800

    fix: 字典删除

--- src/axios/api/dic/index.ts
+++ src/axios/api/dic/index.ts
@@ -4,7 +4,7 @@ import { get, put, post, del } from '../../http';
 export const getDicAll = (data: any) => get({ url: '/dict/', data }); // 获取字典列表
 export const postDic = (data: any) => post({ url: '/dict/', data }); // 插入字典词条
 export const updateDic = (data: any) => put({ url: '/dict/', data }); // 更新字典
-export const delDic = (data: any) => del({ url: '/dict/' + data }); // 删除字典
+export const delDic = (data: any) => del({ url: '/dict/' + data.id, data }); // 删除字典
 
 // 字典数据
 export const getDicByKey = (data: any) => get({ url: '/dictData/dict/' + data.dictId, data }); // 查询单条字典信息

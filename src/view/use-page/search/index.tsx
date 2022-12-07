590b047c039b9d6f25b3e8e6eb05497bb61c4dce
张章 <hzzhangzhang1@corp.netease.com>
2022-12-07 17:24:58 +0800

    feat: 磨煤机demo

--- src/view/use-page/search/index.tsx
+++ src/view/use-page/search/index.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
-import { Card, Input, Collapse } from 'antd';
+import { Card, Input, Collapse, Row, Col } from 'antd';
 import { getTripleTreeData } from '@/axios';
 import Graph from '../componets/graph';
+import { config } from './config';
 import './index.less';
 
 const { Panel } = Collapse;
@@ -15,7 +16,7 @@ export default function HighSearch() {
   };
   return (
     <div className="m-high-graph-page">
-      <Card title={<Input.Search placeholder="智能查询，如：磨煤机" allowClear onSearch={onSearch} style={{ width: 200 }} />}>
+      <Card title={<Input.Search placeholder="智能查询，如：磨煤机" allowClear onSearch={onSearch} style={{ width: 400 }} />}>
         <div className="content">
           <Graph func={getTripleTreeData} refresh={false} keyword={keyword} callback={v => setList(v)} />
 
@@ -26,7 +27,11 @@ export default function HighSearch() {
                 <Collapse accordion>
                   {list?.map((item, index) => (
                     <Panel header={Object.keys(item)[0] + ',' + Object.values(item)[0]} key={Object.keys(item)[0]}>
-                      <div className="regulation" />
+                      <Row>
+                        {config[Object.keys(item)[0]].map(item => (
+                          <Col span={6}>{item}</Col>
+                        ))}
+                      </Row>
                     </Panel>
                   ))}
                 </Collapse>

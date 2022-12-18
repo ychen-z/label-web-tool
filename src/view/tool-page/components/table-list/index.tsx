56e539ca2302c7a36fc30a49c7b81f2c01e1b2c8
张章 <hzzhangzhang1@corp.netease.com>
2022-12-18 10:35:49 +0800

    增加轮询

--- src/view/tool-page/components/table-list/index.tsx
+++ src/view/tool-page/components/table-list/index.tsx
@@ -1,104 +1,115 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import { Table } from 'antd';
 import { TablePaginationConfig } from 'antd/es/table';
+import { useInterval } from 'ahooks';
 import { getSampleData } from '@/axios';
 import useFetch from '@/hooks/common/useFetch';
 import './index.less';
 
 function TableList(props) {
-    const { type, shouldUpdate, textType } = props;
-    const DEFAULT_PARAM = {
-        type: type || 'model', // model—模型抽样 pre—预处理抽样
-        page: 1,
-        textType,
-        size: 10
+  const { type, shouldUpdate, textType } = props;
+  const [interval, setInterval] = useState<null | number>(null);
+
+  const DEFAULT_PARAM = {
+    type: type || 'model', // model—模型抽样 pre—预处理抽样
+    page: 1,
+    textType,
+    size: 10
+  };
+
+  const columns = [
+    {
+      title: '语料',
+      dataIndex: 'text',
+      key: 'text'
+    },
+    {
+      title: '识别结果',
+      dataIndex: 'textMark',
+      key: 'textMark',
+      render: (text, record) => {
+        return (
+          <>
+            <div dangerouslySetInnerHTML={{ __html: text || record.text }} />
+            {!!record.relations?.length &&
+              record.relations.map(item => (
+                <div className="u-desc" style={{ color: item.color }}>
+                  <span className="title">头实体: </span>
+                  {item.headEntity}； <span className="title">尾实体: </span>
+                  {item.tailEntity}；<span className="title">关系: </span> {item.dictName}
+                </div>
+              ))}
+          </>
+        );
+      }
+    }
+  ];
+
+  const [pagination, setPagination] = useState({
+    page: 1,
+    total: 1
+  });
+
+  const [list, setList] = useState([]);
+
+  const [params, setParams] = useState(DEFAULT_PARAM);
+
+  const { dispatch: fetchTemplateList, isLoading } = useFetch(getSampleData, params, false);
+
+  const getList = useCallback(
+    (search: {}) => {
+      const tempParams = Object.assign({}, params, { page: 1 }, search);
+      setParams(tempParams); // 记录上一次搜索记录
+      fetchTemplateList(tempParams).then(res => {
+        setList(res.content);
+        setPagination({ page: tempParams.page, total: res.totalElements }); // 记录分页器参数
+      });
+    },
+    [fetchTemplateList, params]
+  );
+
+  const handleTableChange = (pagination: TablePaginationConfig) => {
+    const tempParams = {
+      size: pagination.pageSize,
+      page: pagination.current
     };
+    getList(tempParams);
+  };
 
-    const columns = [
-        {
-            title: '语料',
-            dataIndex: 'text',
-            key: 'text'
-        },
-        {
-            title: '识别结果',
-            dataIndex: 'textMark',
-            key: 'textMark',
-            render: (text, record) => {
-                return (
-                    <>
-                        <div dangerouslySetInnerHTML={{ __html: text || record.text }} />
-                        {!!record.relations?.length &&
-                            record.relations.map(item => (
-                                <div className="u-desc" style={{ color: item.color }}>
-                                    <span className="title">头实体: </span>
-                                    {item.headEntity}； <span className="title">尾实体: </span>
-                                    {item.tailEntity}；<span className="title">关系: </span> {item.dictName}
-                                </div>
-                            ))}
-                    </>
-                );
-            }
-        }
-    ];
+  useInterval(() => {
+    getList({});
+  }, interval);
 
-    const [pagination, setPagination] = useState({
-        page: 1,
-        total: 1
-    });
+  useEffect(() => {
+    getList({});
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    const [list, setList] = useState([]);
+  useEffect(() => {
+    if (shouldUpdate) {
+      // 识别成功后，查询列表
+      setInterval(5000);
+    }
 
-    const [params, setParams] = useState(DEFAULT_PARAM);
-
-    const { dispatch: fetchTemplateList, isLoading } = useFetch(getSampleData, params, false);
-
-    const getList = useCallback(
-        (search: {}) => {
-            const tempParams = Object.assign({}, params, { page: 1 }, search);
-            setParams(tempParams); // 记录上一次搜索记录
-            fetchTemplateList(tempParams).then(res => {
-                setList(res.content);
-                setPagination({ page: tempParams.page, total: res.totalElements }); // 记录分页器参数
-            });
-        },
-        [fetchTemplateList, params]
-    );
-
-    const handleTableChange = (pagination: TablePaginationConfig) => {
-        const tempParams = {
-            size: pagination.pageSize,
-            page: pagination.current
-        };
-        getList(tempParams);
+    return () => {
+      setInterval(null);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [shouldUpdate]);
 
-    useEffect(() => {
-        getList({});
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-
-    useEffect(() => {
-        if (shouldUpdate) {
-            // 识别成功后，查询列表
-            getList({});
-        }
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [shouldUpdate]);
-
-    return (
-        <div className="u-table-list">
-            <Table
-                loading={isLoading}
-                rowKey="id"
-                dataSource={list}
-                columns={columns}
-                onChange={handleTableChange}
-                tableLayout="fixed"
-                pagination={{ current: pagination.page, total: pagination.total, showQuickJumper: true, showSizeChanger: true }}
-            />
-        </div>
-    );
+  return (
+    <div className="u-table-list">
+      <Table
+        loading={isLoading}
+        rowKey="id"
+        dataSource={list}
+        columns={columns}
+        onChange={handleTableChange}
+        tableLayout="fixed"
+        pagination={{ current: pagination.page, total: pagination.total, showQuickJumper: true, showSizeChanger: true }}
+      />
+    </div>
+  );
 }
 
 export default TableList;
--- src/view/tool-page/model/training-model.tsx
+++ src/view/tool-page/model/training-model.tsx
@@ -1,6 +1,7 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { Form, Input, Button, Select, message } from 'antd';
 import ReactEcharts from 'echarts-for-react';
+import { useInterval } from 'ahooks';
 import { GlobalContext } from '@/context';
 import useFetch from '@/hooks/common/useFetch';
 import { postTrainModel, getCurrentRate, getHistoryRates } from '@/axios';
@@ -11,131 +12,143 @@ const { Option } = Select;
  * @returns 模型训练
  */
 export default function TrainingModel(props) {
-    const { textType } = props;
-    const { refreshState } = useContext(GlobalContext);
-    const { dispatch: dispatchPostTrainModel, isLoading } = useFetch(postTrainModel, { page: 1, textType, size: Infinity }, false);
-    const { data: currentRateData, dispatch: dispatchGetCurrentRate } = useFetch(getCurrentRate, { textType });
-    const { data: historyRateData, dispatch: dispatchGetHistoryRates } = useFetch(getHistoryRates, { textType });
+  const { textType } = props;
+  const { refreshState } = useContext(GlobalContext);
+  const { dispatch: dispatchPostTrainModel, isLoading } = useFetch(postTrainModel, { page: 1, textType, size: Infinity }, false);
+  const { data: currentRateData, dispatch: dispatchGetCurrentRate } = useFetch(getCurrentRate, { textType });
+  const { data: historyRateData, dispatch: dispatchGetHistoryRates } = useFetch(getHistoryRates, { textType });
+  const [interval, setInterval] = useState<null | number>(null);
 
-    const onFinish = values => {
-        console.log('训练');
-        var dictIds = localStorage
-            .getItem('dictIds-' + textType)
-            ?.split(',')
-            .map(item => (item = Number(item)));
-        var textIds = localStorage
-            .getItem('textIds-' + textType)
-            ?.split(',')
-            .map(item => (item = Number(item)));
+  useInterval(() => {
+    refreshState();
+    dispatchGetCurrentRate({ textType });
+    dispatchGetHistoryRates({ textType });
+  }, interval);
 
-        dispatchPostTrainModel({ ...values, textIds, dictIds, textType }).then(res => {
-            message.success('训练成功');
-            refreshState();
-            dispatchGetCurrentRate({ textType });
-            dispatchGetHistoryRates({ textType });
-        });
+  const onFinish = values => {
+    console.log('训练');
+    var dictIds = localStorage
+      .getItem('dictIds-' + textType)
+      ?.split(',')
+      .map(item => (item = Number(item)));
+    var textIds = localStorage
+      .getItem('textIds-' + textType)
+      ?.split(',')
+      .map(item => (item = Number(item)));
+
+    dispatchPostTrainModel({ ...values, textIds, dictIds, textType }).then(res => {
+      message.success('训练成功');
+      setInterval(5000);
+    });
+  };
+
+  const getCurrentRateOption = data => {
+    if (!data) return {};
+    return {
+      title: { text: '当前训练准确率 %' },
+      xAxis: {
+        type: 'category'
+      },
+      yAxis: {
+        type: 'value',
+        min: 80
+      },
+      color: '#7cb305',
+      grid: {
+        show: true,
+        gridIndex: 1
+      },
+      legend: {
+        show: true
+      },
+      tooltip: {
+        show: true
+      },
+      series: [
+        {
+          data: data.map(item => (item = item.toFixed(2))),
+          type: 'line'
+        }
+      ]
     };
+  };
 
-    const getCurrentRateOption = data => {
-        if (!data) return {};
-        return {
-            title: { text: '当前训练准确率 %' },
-            xAxis: {
-                type: 'category'
-            },
-            yAxis: {
-                type: 'value',
-                min: 80
-            },
-            color: '#7cb305',
-            grid: {
-                show: true,
-                gridIndex: 1
-            },
-            legend: {
-                show: true
-            },
-            tooltip: {
-                show: true
-            },
-            series: [
-                {
-                    data: data.map(item => (item = item.toFixed(2))),
-                    type: 'line'
-                }
-            ]
-        };
+  const getHistoryRateOption = data => {
+    if (!data) return {};
+    return {
+      title: { text: '历史训练准确率 %' },
+      xAxis: {
+        type: 'category',
+        axisLabel: {
+          show: true,
+          interval: 1
+        }
+      },
+      yAxis: {
+        type: 'value',
+        min: 80
+      },
+      grid: {
+        show: true
+      },
+      color: '#9254de',
+      legend: {
+        show: true
+      },
+      tooltip: {
+        show: true
+      },
+      series: [
+        {
+          data: data.map(item => (item = item.rate.toFixed(2))),
+          type: 'line'
+        }
+      ]
     };
+  };
 
-    const getHistoryRateOption = data => {
-        if (!data) return {};
-        return {
-            title: { text: '历史训练准确率 %' },
-            xAxis: {
-                type: 'category',
-                axisLabel: {
-                    show: true,
-                    interval: 1
-                }
-            },
-            yAxis: {
-                type: 'value',
-                min: 80
-            },
-            grid: {
-                show: true
-            },
-            color: '#9254de',
-            legend: {
-                show: true
-            },
-            tooltip: {
-                show: true
-            },
-            series: [
-                {
-                    data: data.map(item => (item = item.rate.toFixed(2))),
-                    type: 'line'
-                }
-            ]
-        };
+  useEffect(() => {
+    return () => {
+      setInterval(null);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-    return (
-        <div className="m-training-model">
-            <section className="u-form">
-                <Form name="basic" initialValues={{ model: 'BiLSTM' }} labelCol={{ span: 8 }} wrapperCol={{ span: 15 }} onFinish={onFinish}>
-                    <Form.Item label="模型" name="model" style={{ width: 300 }} rules={[{ required: true, message: '请输入' }]}>
-                        <Select>
-                            <Option value="BiLSTM">BiLSTM</Option>
-                        </Select>
-                    </Form.Item>
-                    <Form.Item label="训练数量" name="clusterCount">
-                        <Input />
-                    </Form.Item>
+  return (
+    <div className="m-training-model">
+      <section className="u-form">
+        <Form name="basic" initialValues={{ model: 'BiLSTM' }} labelCol={{ span: 8 }} wrapperCol={{ span: 15 }} onFinish={onFinish}>
+          <Form.Item label="模型" name="model" style={{ width: 300 }} rules={[{ required: true, message: '请输入' }]}>
+            <Select>
+              <Option value="BiLSTM">BiLSTM</Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="训练数量" name="clusterCount">
+            <Input />
+          </Form.Item>
 
-                    <Form.Item label="输入维度" name="vectorScale">
-                        <Input />
-                    </Form.Item>
+          <Form.Item label="输入维度" name="vectorScale">
+            <Input />
+          </Form.Item>
 
-                    <Form.Item>
-                        <Button type="primary" htmlType="submit" loading={isLoading}>
-                            训练
-                        </Button>
-                    </Form.Item>
-                </Form>
-            </section>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" loading={isLoading}>
+              训练
+            </Button>
+          </Form.Item>
+        </Form>
+      </section>
 
-            <section className="u-view">
-                <div className="u-view-left">
-                    <ReactEcharts option={getCurrentRateOption(currentRateData)} />
-                    <div>迭代次数</div>
-                </div>
-                <div className="u-view-right">
-                    <ReactEcharts option={getHistoryRateOption(historyRateData)} />
-                    <div>训练序号</div>
-                </div>
-            </section>
+      <section className="u-view">
+        <div className="u-view-left">
+          <ReactEcharts option={getCurrentRateOption(currentRateData)} />
+          <div>迭代次数</div>
         </div>
-    );
+        <div className="u-view-right">
+          <ReactEcharts option={getHistoryRateOption(historyRateData)} />
+          <div>训练序号</div>
+        </div>
+      </section>
+    </div>
+  );
 }
